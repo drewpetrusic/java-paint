fcd900c907a64899a4a2a1deef6aa45c4d93a462
drewpetrusic <57770953+drewpetrusic@users.noreply.github.com>
2021-02-07 05:19:17 -0600

    Update Curve.java

--- src/shapes/Curve.java
+++ src/shapes/Curve.java
@@ -11,41 +11,24 @@ public class Curve extends Double {
 	private static final long serialVersionUID = 1L;
 	private Color c;
 
-	// One constructor takes x,y,and a Color. This invokes moveTo with x and y and initializes the field C.
-	// The other constructor parses the string data initializing the field c, invokes moveTo with
-	// the first two ints, then iterates over the remaining data invoking lineTo with each data pair.
-	
-	
-	public Curve(int x, int y, Color c) { //Constructor takes x, y, and Color.  invokes moveTo with x and y and initializes field C.
+	public Curve(int x, int y, Color c) {
 		super.moveTo(x, y);
 		this.c = c;
 	}
-					
-	
-	
-	
-	
-	public Curve(String line) { //constructor two - wrong
+
+	public Curve(String line) {
 		Scanner sc = new Scanner(line.strip());
 		sc.useDelimiter(",\\s*");
 		String type = sc.next();
-									// <-- parses the string data initializing field c, invokes moveTo with the first two ints
-									// then iterates over the remaining data invoking lineTo with each data pair.
-									// pretty much: do a while loop  "moveTo" for the first pair, then "lineTo" for the rest of points
+
 		int rgb = sc.nextInt();
 		c = new Color(rgb);
 		super.moveTo(sc.nextInt(), sc.nextInt());
-		while(sc.hasNextInt()) {
+		while (sc.hasNextInt()) {
 			super.lineTo(sc.nextInt(), sc.nextInt());
 		}
 		sc.close();
 	}
-	
-	
-	
-	
-	
-	
 
 	public String toString() {
 		String typeColor = "curve," + c.getRGB();
@@ -63,7 +46,7 @@ public class Curve extends Double {
 		}
 		return String.format("%s%s%n", typeColor, xy);
 	}
-	
+
 	public Color getDrawColor() {
 		return c;
 	}
@@ -71,10 +54,7 @@ public class Curve extends Double {
 	public static void main(String[] args) {
 		Curve e = new Curve("curve, -16776961, 475, 325, 38, 40, 49, 50,78,100"); // deleted \n off
 		System.out.print(e);
-	
 
 	}
-
-
 
 }
